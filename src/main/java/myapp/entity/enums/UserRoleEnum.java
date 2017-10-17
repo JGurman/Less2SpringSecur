79e84d4c92b3ac3cbdd79adf875e9520b7174a13
JGruman <pereverzyev2517@gmail.com>
2017-10-17 20:47:29 +0300

    asdfsd

---
+++ src/main/java/myapp/entity/enums/UserRoleEnum.java
@@ -0,0 +1,10 @@
+package myapp.entity.enums;
+
+public enum UserRoleEnum {
+    ADMIN,
+    USER,
+    ANONYMOUS;
+
+    UserRoleEnum() {
+    }
+}
---
+++ src/main/java/myapp/service/UserService.java
@@ -0,0 +1,7 @@
+package myapp.service;
+import com.________.entity.User;
+
+public interface UserService {
+    User getUser(String login);
+}
+
